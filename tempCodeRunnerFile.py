02a5e33badb7bbda99339376a3946d91f3eeb741
ramiadi <ramin2004@hotmail.no>
2025-07-27 09:32:37 +0200

    border gaps and teleportation
    added border gaps at the middle left and right. when a player enters the gap, he teleports to the opposite side of the gap

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+  # # Check for teleportation (calculate gap_y same way as in border_wall method)
+    # gap_y = (VINDU_HOYDE // 2 // grid.blockSize) * grid.blockSize
+    # pacman.check_teleportation(VINDU_HOYDE, VINDU_BREDDE, gap_y, grid.blockSize)
