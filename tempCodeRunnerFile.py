aed3251e4682bc3a4eeba74f4385783cf0e00384
ramiadi <ramin2004@hotmail.no>
2025-07-30 10:23:17 +0200

    new background and border
    Implemented a method to check if walls are adjacent to each other, so they visually connect. Instead of giving every wall a border on all sides, the method checks if neighboring walls exist and only draws borders where necessary. This way, connected walls form a continuous structure.

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,5 @@
-  # # Check for teleportation (calculate gap_y same way as in border_wall method)
-    # gap_y = (VINDU_HOYDE // 2 // grid.blockSize) * grid.blockSize
-    # pacman.check_teleportation(VINDU_HOYDE, VINDU_BREDDE, gap_y, grid.blockSize)
+# Add slight 3D effect with lighter blue on top and left edges
+        # light_blue = (100, 100, 255)
+        # pg.draw.line(screen, light_blue, (self.x + 2, self.y + 2), (self.x + self.width - 3, self.y + 2), 1)  # Top edge
+        # pg.draw.line(screen, light_blue, (self.x + 2, self.y + 2), (self.x + 2, self.y + self.height - 3), 1)  # Left edge
+        
